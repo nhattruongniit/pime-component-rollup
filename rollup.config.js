514cb3eb26a3908b9a75d019fd4c51d40c9c3b67
truong.nn <truong.nn@amanotes.com>
2024-03-14 15:53:44 +0700

    init

---
+++ rollup.config.js
@@ -0,0 +1,19 @@
+import typescript from "@rollup/plugin-typescript";
+import url from "@rollup/plugin-url";
+
+export default {
+  input: "src/index.ts",
+  output: [
+    {
+      file: "dist/index.cjs.js",
+      format: "cjs",
+      sourcemap: true,
+    },
+    {
+      file: "dist/index.esm.js",
+      format: "es",
+      sourcemap: true,
+    },
+  ],
+  plugins: [url(), typescript()],
+};
---
+++ src/components/table/table.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Table() {
+  return <div>welcome to table</div>;
+}
+
+export default Table;
